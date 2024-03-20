28bbb6049683349cfe0731b071eaf41ad5e0d452
Mayank mehta <mayankmehta1327@gmail.com>
2024-03-20 13:00:52 +0530

    createTodoFeture

---
+++ app.js
@@ -0,0 +1,26 @@
+let list = [];
+let req = prompt("please enter your req");
+
+while(true) {
+    if(req == "quit") {
+        console.log("quiiting app");
+        break;
+    }
+    if(req == "list") {
+    for(let i=0; i<list.length; i++) {
+        console.log(i,list[i]);
+    }
+        console.log("list was printed");
+    } else if(req == "add") {
+       let task = prompt("please enter your task");
+       list.push(task);
+       console.log('task added');
+    } else if(req == "delete") {
+      let idx =  prompt("please enter the task index");
+         list.splice(idx,1);
+         console.log("task deleted");
+     }else {
+       console.log("please enter correct req");
+     }
+      req = prompt("please enter your req")
+}
